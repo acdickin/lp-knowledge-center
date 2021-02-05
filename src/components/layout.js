44a0238c56c56d3c45413d6d4295f271d776f9cf
Andrew Cosmic <acosmic@liveperson.com>
2021-02-05 14:41:05 -0800

    fixed some style issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
       <Header />
       <div style={page}>
         <Sidebar />
-        <main>{children}</main>
+        {children}
       </div>
       <Footer />
     </>
