cd7b8e35e685b16268f4f42bd2308c0626303927
Andrew Cosmic <acosmic@liveperson.com>
2021-02-08 14:44:25 -0800

    need to get the title postion instead of using a number

--- src/components/jump-to.js
+++ src/components/jump-to.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react"
 import $ from "jquery";
 import scrollTo from 'gatsby-plugin-smoothscroll';
 
-const JumpTo = () => {
-
+const JumpTo = (props) => {
+  console.log(props)
   const [anchorList, setAnchorList] = useState([])
 
   const populateAnchors = () => {
@@ -18,10 +18,39 @@ const JumpTo = () => {
   useEffect(() => {
     populateAnchors();
   }, [])
+  useEffect(() => {
+    $(window).scroll(function () {
+      //check the window's position and account for the header
+      var position = $(this).scrollTop() + 140; // TODO FIX THIS
+      var titles = document.getElementsByTagName('h2');
+      //for each h2 in the article
+      $.each(titles, function () {
+        // get its position and id
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
+        //if the position of the window is greater than the position of the title (that is, the title has scrolled out of view)
+        if (position >= target) {
+          //deactivate all other active links in the anchorlist
+          $('.anchorlist > ul > li > a').removeClass('active');
+          //find the matching link in the anchorlist
+          var current = $('a[href="#' + id + '"]');
+          //set it to active
+          current.addClass('active');
+        } else if (position === 140) { // TODO FIX THIS
+          $('.anchorlist > ul > li > a').removeClass('active');
+          $('.anchorlist > ul > #jumptotop > a').addClass('active');
+        }
+      });
+      if ($(window).scrollTop() + $(window).height() === $(document).height()) {
+        $('.anchorlist > ul > li ').removeClass('active');
+        $('.anchoritem').last().addClass('active');
+      };
+    });
+  })
 
   const renderAnchorlist = () => {
     console.log(anchorList)
-    return anchorList.map(anchor => <li key={anchor.id} className="anchoritem" tabIndex="-1" onClick={() => { console.log("scroll"); scrollTo("#" + anchor.id) }}>{anchor.text}</li>)
+    return anchorList.map(anchor => <li key={anchor.id} className="anchoritem" tabIndex="-1" onClick={() => { console.log("scroll"); scrollTo("#" + anchor.id) }}><a>{anchor.text}</a></li>)
   }
 
   const jumptoList = {
@@ -31,7 +60,9 @@ const JumpTo = () => {
 
   return (
     <div className="jumpto">
-      <ul style={jumptoList}>
+      <ul style={jumptoList} className="anchorlist">
+
+        {(anchorList.length > 0) ? <li id="jumptotop"><a className="active" href="#">{props.title}</a></li> : ""}
         {(anchorList.length > 0) ? renderAnchorlist() : ""}
       </ul>
     </div>
--- src/components/page.js
+++ src/components/page.js
@@ -6,14 +6,13 @@ import Layout from "./layout"
 const Page = (props) => {
   const { body, title, why_the_product_is_useful, post_tags } = props.pageContext.elements
   const { breadCrumbs } = props.pageContext;
-
   return (
     <Layout>
       <div className="main-content">
         <ContentHeader breadCrumbs={breadCrumbs} title={title} why_the_product_is_useful={why_the_product_is_useful} post_tags={post_tags} />
         <div dangerouslySetInnerHTML={{ __html: body.value }} />
       </div>
-      <JumpTo />
+      <JumpTo title={title.value} />
     </Layout>
   )
 }
