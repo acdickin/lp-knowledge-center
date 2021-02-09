77d5429f80b944d4b3efaaaf7fab48f0e1401576
Andrew Cosmic <acosmic@liveperson.com>
2021-02-09 13:26:04 -0800

    active state for jump to is working again

--- src/components/jump-to.js
+++ src/components/jump-to.js
@@ -12,48 +12,45 @@ const JumpTo = (props) => {
     let anchorlinks = document.getElementsByTagName("h2");
     $.each(anchorlinks, function () {
       $(this).attr('id', $(this).text().replace(/[\W_]/g, "-").toLowerCase())
-      setAnchorList(preArray => [...preArray, { text: $(this).text(), id: $(this).attr("id") }])
+      let position = $(this).offset().top
+      setAnchorList(preArray => [...preArray, { text: $(this).text(), id: $(this).attr("id"), pos: position }])
     })
   }
-
+  // this only runs once on page load
   useEffect(() => {
     populateAnchors();
+  }, [])
+
+  useEffect(() => {
     $(window).scroll(function () {
-      const mainTitlePostion = $('#maintitle').offsetTop;
+      const mainTitlePostion = $('#maintitle').offset().top;
+      console.log(mainTitlePostion)
       //check the window's position and account for the header
-      let position = $(this).scrollTop() + mainTitlePostion;
-      let titles = document.getElementsByTagName('h2');
-
+      let position = $(this).scrollTop();
+      console.log(position);
       //for each h2 in the article
-      $.each(titles, function () {
-        // get its position and id
-        let target = $(this).offsetTop;
-
-        let id = $(this).attr('id');
+      anchorList.forEach(anchor => {
         //if the position of the window is greater than the position of the title (that is, the title has scrolled out of view)
-        if (position >= target) {
+        if (position > anchor.pos) {
           //deactivate all other active links in the anchorlist
-          $('.anchorlist > ul > li ').removeClass('active');
-          //find the matching link in the anchorlist
-          let current = $('a[href="#' + id + '"]');
-          //set it to active
-          current.addClass('active');
-        } else if (position === mainTitlePostion) {
-          $('.anchorlist > ul > li ').removeClass('active');
-          $('.anchorlist > ul > #jumptotop ').addClass('active');
+          $('.anchorlist > li ').removeClass('active');
+          $(`li[name=${anchor.id}]`).addClass('active')
+        } else if (position <= mainTitlePostion) {
+          $('.anchorlist > li ').removeClass('active');
+          $('.anchorlist > #jumptotop ').addClass('active');
         }
       });
       if ($(window).scrollTop() + $(window).height() === $(document).height()) {
-        $('.anchorlist > ul > li ').removeClass('active');
+        $('.anchorlist > li').removeClass('active');
         $('.anchoritem').last().addClass('active');
       };
     });
-  }, [])
+  })
 
   const renderAnchorlist = () => {
     console.log(anchorList)
     // cant have a tag as it breaks the onclick
-    return anchorList.map(anchor => <li key={anchor.id} className="anchoritem" tabIndex="-1" onClick={() => { scrollTo("#" + anchor.id) }}>{anchor.text}</li>)
+    return anchorList.map(anchor => <li key={anchor.id} className="anchoritem" tabIndex="-1" name={anchor.id} onClick={() => { scrollTo("#" + anchor.id) }}>{anchor.text}</li>)
   }
 
   const jumptoList = {
