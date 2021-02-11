2c440bff8c30a2afe7ca28ba1a11c14fe97ba8d2
Andrew Cosmic <acosmic@liveperson.com>
2021-02-11 14:52:20 -0800

    creating search box and functionallity

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 // import { Link } from "gatsby"
 
 import React from "react"
+import Search from "./search"
+const searchIndices = [{ name: `Pages`, title: `Pages` }]
 
 const Header = () => {
   return (
@@ -11,7 +13,8 @@ const Header = () => {
       <div id="logocontainer">
         <a href="/"><img src="img/lp-logo.svg" /></a>
       </div>
-      <input type="text" id="aa-search-input" className="aa-input-search" placeholder="Search our knowledge center..." name="search" autoComplete="on" />
+      <Search indices={searchIndices} />
+      {/* <input type="text" id="aa-search-input" className="aa-input-search" placeholder="Search our knowledge center..." name="search" autoComplete="on" /> */}
       <div id="homebuttons">
         <span id="login" className="homebutton"><a target="_blank" href="https://liveengage.liveperson.net">LivePerson login</a></span>
       </div>
