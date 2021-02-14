6b4061816633167b441d253d72d1f214c43ba5d6
itay haephrati <itay1313@gmail.com>
2021-02-14 18:31:13 +0200

    example of tailwind component can be removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div>
 {/* Start of tailwind component */}
-
-
 <div className="flex flex-col bg-white w-full">
   <div id="chat"  className="flex flex-col mt-2 flex-col-reverse overflow-y-scroll	 space-y-3 mb-20 pb-3 ">
     
@@ -20,16 +18,16 @@ const IndexPage = () => (
       2/10
     </div>
     <div className="w-max ml-auto break-all mt-2 mb-1 p-2 rounded-br-none bg-blue-500 rounded-2xl text-white text-left mr-5">
-      But numbers can
+      Yea like
     </div>
     <div className="other break-all mt-2  ml-5 rounded-bl-none float-none bg-gray-300 mr-auto rounded-2xl p-2">
       Aww thx!!
     </div>
     <div className="w-max ml-auto break-all mt-2 mb-1 p-2 rounded-br-none bg-blue-500 rounded-2xl text-white text-left mr-5">
-      Words can't describe how beautiful you are :)
+      Thanks, It looks really good
     </div>
      <div className="other break-all mt-2  ml-5 rounded-bl-none float-none bg-gray-300 mr-auto rounded-2xl p-2">
-      Words can't decsribe how beautiful you are ;)
+      Welcome to our new Liveperson knowledge center ;)
     </div>   
   </div>
   <div className="flex flex-row  items-center  bottom-0 my-2 w-full">
