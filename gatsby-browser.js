3bacc3e5816cd85336b32ebdfd858d7815e66d32
Andrew Cosmic <acosmic@liveperson.com>
2021-02-22 14:09:57 -0800

    fixing en pages. added a language json which will be implemented later

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,8 +13,9 @@ exports.onClientEntry = () => {
   console.log("path: ", urlArray)
   const page = urlArray[urlArray.length - 2]
   console.log("page:", page);
-
-  if (urlArray.length > 2 && urlArray[1] !== lang && (lang !== "en-US" || lang !== "en")) {
+  console.log(lang)
+  // TODO: Handle languages we dont have, make sure a 404 page is created in KONTENT
+  if (urlArray.length > 2 && urlArray[1] !== lang && lang !== "en-US") {
     const url = `/${lang}/${page}/`
     console.log("url", url)
     window.location.pathname = `${url}`
